810ca39024050d64ab2a5b0bf37a17254f121192
Bart Massey <bart.massey@gmail.com>
2021-01-19 18:04:04 -0800

    fixed edge count checking misfeature

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,8 @@ pub fn read_graph<R: Read>(graph_file: R) -> Graph {
     assert_eq!(header[0], "p");
     assert_eq!(header[1], "edge");
     let nnodes: usize = header[2].parse().unwrap();
-    let mut nedges: usize = header[3].parse().unwrap();
+    let nedges: usize = header[3].parse().unwrap();
+    let mut nedges_seen = 0;
     for line in lines {
         let edge: Vec<&str> = line
             .split_whitespace()
@@ -38,10 +39,11 @@ pub fn read_graph<R: Read>(graph_file: R) -> Graph {
         let n2: u64 = edge[2].parse().unwrap();
         adj(n1, n2);
         adj(n2, n1);
-        assert!(nedges > 0);
-        nedges -= 2;
+        nedges_seen += 1;
     }
-    assert_eq!(nedges, 0);
+    // XXX Some graphs count two edges per undirected edge,
+    // some count one.
+    assert!(nedges == nedges_seen || 2 * nedges == nedges_seen);
     assert!(nnodes >= result.len());
     result
 }
